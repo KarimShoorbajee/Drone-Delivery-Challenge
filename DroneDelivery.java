e71de6ebe22c4717b71466940bcc2f156a4008bf
karimshoorbajee <karimshoorbajee99@gmail.com>
2019-02-14 20:54:42 -0500

    Working line reader

--- DroneDelivery.java
+++ DroneDelivery.java
@@ -8,8 +8,33 @@
  * 9-10 promoter
  * (10 - score) is number of hours to earn that score
  */
+import java.io.*;
+
 public class DroneDelivery {
-    public static void main (String [] args) {
+    public static void main (String [] args) throws IOException {
+
+        if (args.length < 1) {
+            System.out.println("Invalid input");
+            System.exit(-1);
+        }
+
+        File input = null;
+        BufferedReader reader = null;
+        try {
+            input = new File(args[0]);
+            reader = new BufferedReader(new FileReader(input));
+        }
+        catch (Exception e){
+            System.out.println("File Not Found.");
+            System.exit(-1);
+        }
+        
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
         Drone d1 = new Drone(1);
         d1.printCoords();
         d1.moveEast();
