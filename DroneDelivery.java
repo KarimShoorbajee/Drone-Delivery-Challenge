f903d6b57d62344254c2af997f8964e7c6761f67
karimshoorbajee <karimshoorbajee99@gmail.com>
2019-02-14 19:49:55 -0500

    Adding git ignore

---
+++ DroneDelivery.java
@@ -0,0 +1,15 @@
+/**
+ * 1 drone
+ * All deliveries originate at origin
+ * This means that drone has to go back after each delivery
+ * Speed is one gridblock/min
+ * 0-6 detractor
+ * 7-8 neutral
+ * 9-10 promoter
+ * (10 - score) is number of hours to earn that score
+ */
+public class DroneDelivery {
+    public static void main (String [] args) {
+        System.out.println("Hello World");
+    }
+}
